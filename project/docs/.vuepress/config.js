0cfb4103e54c2a7cea3d7e97a03b97b4b780d6bc
cuehuabuleng <1079713849@qq.com>
2020-12-02 16:44:30 +0800

    feat:整个网站的基础架构已经完成，后续添加文章或者更改主题样式

--- project/docs/.vuepress/config.js
+++ project/docs/.vuepress/config.js
@@ -8,17 +8,117 @@ module.exports = {
         logo: '/02.png',
         nav: [
             { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'External', link: 'https://google.com' },
+            { text: 'github', link: 'https://github.com/cuehuabuleng' },
+            { text: 'personal-website', link: 'http://114.55.93.74:8009/#/home' }
+            // {
+            //     text: 'Languages',
+            //     ariaLabel: 'Language Menu',
+            //     items: [
+            //         { text: 'Chinese', link: '/language/chinese/' },
+            //         { text: 'Japanese', link: '/language/japanese/' }
+            //     ]
+            // }
+        ],
+        sidebar: [
             {
-                text: 'Languages',
-                ariaLabel: 'Language Menu',
-                items: [
-                    { text: 'Chinese', link: '/language/chinese/' },
-                    { text: 'Japanese', link: '/language/japanese/' }
+                title: 'HTML',
+                collapsable: false,
+                children: [
+                    'pages/HTML/',
+                    'pages/HTML/DOM'
+                ]
+            },
+            {
+                title: 'css',
+                collapsable: true,
+                children: [
+                    'pages/css/'
+                ]
+            },
+            {
+                title: 'js基础',
+                collapsable: true,
+                children: [
+                    {
+                        title: 'es5',
+                        collapsable: true,
+                        children: [
+                            'pages/js/es5/',
+                            'pages/js/es5/this的五种绑定'
+                        ]
+                    },
+                    {
+                        title: 'es6',
+                        collapsable: true,
+                        children: [
+                            'pages/js/es6/'
+                        ]
+                    }
+                ]
+            },
+            {
+                title: 'vue',
+                collapsable: true,
+                children: [
+                    'pages/vue/'
+                ]
+            },
+            {
+                title: 'react',
+                collapsable: true,
+                children: [
+                    'pages/react/'
+                ]
+            },
+            {
+                title: 'node',
+                collapsable: true,
+                children: [
+                    'pages/node/'
+                ]
+            },
+            {
+                title: 'webpack',
+                collapsable: true,
+                children: [
+                    'pages/webpack/'
+                ]
+            },
+            {
+                title: '计算机网络',
+                collapsable: true,
+                children: [
+                    'pages/计算机网络/'
+                ]
+            },
+            {
+                title: '操作系统',
+                collapsable: true,
+                children: [
+                    'pages/操作系统/'
+                ]
+            },
+            {
+                title: '算法',
+                collapsable: true,
+                children: [
+                    'pages/算法/'
+                ]
+            },
+            {
+                title: '前端性能优化',
+                collapsable: true,
+                children: [
+                    'pages/前端性能优化/'
+                ]
+            },
+            {
+                title: '前端工程化',
+                collapsable: true,
+                children: [
+                    'pages/前端工程化/'
                 ]
             }
-        ],
-        sidebar: 'auto'
+        ]
     }
 }
